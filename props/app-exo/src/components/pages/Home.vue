d1112db61e5d1e86343ae0144af9d4e2e8c66314
Akim <gentilakili98@gmail.com>
2022-08-04 22:45:24 +0200

    Commit vuejs

--- props/app-exo/src/components/pages/Home.vue
+++ props/app-exo/src/components/pages/Home.vue
@@ -1,3 +1,9 @@
+
 <template>
-    
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
+    <router-link to="/rapport">Rapport</router-link>
+    <router-link to="/operation">Operation</router-link>
+  </nav>
 </template>
--- props/app-exo/src/main.js
+++ props/app-exo/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import router from './components/routes/routes.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
