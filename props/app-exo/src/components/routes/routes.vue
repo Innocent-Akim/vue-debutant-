d01a8336fe3daf0c24d277ff3de66bfe4c7854b5
Akim <gentilakili98@gmail.com>
2022-08-04 15:58:33 +0200

    ui page

---
+++ props/app-exo/src/components/routes/routes.vue
@@ -0,0 +1,53 @@
+<script>
+import { createRouter, createWebHistory } from "vue-router";
+import dashboard from "../pages/Dashboard.vue";
+import home from "../pages/Home.vue";
+import login from "../pages/Login.vue";
+import rapport from "../pages/Rapport.vue";
+import statisque from "../pages/Statistique.vue";
+import operation from "../pages/Operation.vue";
+import registre from "../pages/Register.vue";
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    components: home,
+  },
+  {
+    name: "dashboard",
+    path: "/dashboard",
+    components: dashboard,
+  },
+  {
+    name: "login",
+    path: "/login",
+    components: login,
+  },
+  {
+    name: "rapport",
+    path: "/rapport",
+    components: rapport,
+  },
+  {
+    name: "registre",
+    path: "/registre",
+    components: registre,
+  },
+  {
+    name: "operation",
+    path: "/operation",
+    components: operation,
+  },
+  {
+    name: "statisque",
+    path: "/statisque",
+    components: statisque,
+  },
+];
+
+export default createRouter({
+  routes: routes,
+  history: createWebHistory(),
+});
+</script>
